7b947651393ba3cc66cbb81a00fff6f52fc01cfe
tmtm8976 <fatemayasser8976@gmail.com>
2025-07-30 11:43:37 +0300

    jail break detection

--- App.tsx
+++ App.tsx
@@ -4,9 +4,13 @@ import BackgroundFetch from 'react-native-background-fetch';
 import { processQueue } from './src/services/syncQueue';
 import { setupBackgroundSync } from './src/services/background';
 import { initQueueTable, resetQueueTable } from './src/db/queue';
+import JailMonkey from 'jail-monkey';
+import { BackHandler } from 'react-native';
 
 export default function App() {
- 
+  if (JailMonkey.isJailBroken()) {
+    BackHandler.exitApp();
+  }
   setupBackgroundSync();
   initQueueTable();
 
